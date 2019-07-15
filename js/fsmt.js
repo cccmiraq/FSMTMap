cec38ae4ac587356155547a7f0e246eac0e4fbaf
CCCM IMO TEAM <cccmiraq@gmail.com>
2019-07-15 14:32:19 +0300

    Update fsmt.js

--- js/fsmt.js
+++ js/fsmt.js
@@ -4,7 +4,7 @@ jQuery(document).ready(function() {
 	$('#left_of_map').hide();
 	$('#analysis').hide();
 
-	var u = "https://ckanviz.swige.unhcr.org/dataset/4506c0af-4a8f-4d01-83a6-8b21f8041084/resource/1b25ff21-4d92-492b-9fc9-01a608f841d6/download/fsmt_sites.csv";
+	var u = "https://ckanviz.swige.unhcr.org/dataset/d4faa190-44ba-410f-bb58-b575513a14b2/resource/05af8930-d7fd-4d48-a552-2b48bef54fdc/download/dcccm-clustercccm-cluster-reporting-toolsfsmt-may-2019fsmt-master-1fsmt-masterdatafsmt_sites.csv";
 	var map, loc, name;
 	d3.csv(u, function(data) {
 		d3.csv("data/fields.csv", function(fields) {
