30ed6e3a1c736f8894ec1d054573bac5140c978c
CCCM IMO TEAM <cccmiraq@gmail.com>
2020-09-28 15:47:47 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 var config = {
 		
 		//URL of the CSV
-		url : "https://ckanviz.swige.unhcr.org/dataset/d4faa190-44ba-410f-bb58-b575513a14b2/resource/5f6f7843-9e2d-447d-a8c7-aaac279e90a2/resource/5f6f7843-9e2d-447d-a8c7-aaac279e90a2/download/dcccm-clustercccm-cluster-reporting-toolsfsmt-may-2019fsmt-master-1fsmt-masterdatafsmt_sites.csv",
+		url : "https://ckanviz.swige.unhcr.org/dataset/d4faa190-44ba-410f-bb58-b575513a14b2/resource/5f6f7843-9e2d-447d-a8c7-aaac279e90a2/resource/5f6f7843-9e2d-447d-a8c7-aaac279e90a2/download/fsmt_sites.csv",
 		
 		//Latitude field
 		lat : "coordinates_latitude",
