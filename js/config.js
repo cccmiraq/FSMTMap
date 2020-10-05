42b17aa67a8c74ca55fd48ec69b594f710677b52
CCCM IMO TEAM <cccmiraq@gmail.com>
2020-10-05 12:03:55 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -49,9 +49,9 @@ var config = {
 				icon : "cluster_shelter_60px_bluebox"
 			},
 			{
-				name : "protection",
-				alias : "Protection",
-				icon : "cluster_protection_60px_bluebox"
+				name : "nfi",
+				alias : "Non-food items",
+				icon : "food_NFI_nonfood_item_60px_bluebox"
 			},
 			{
 				name : "health",
@@ -69,9 +69,9 @@ var config = {
 				//icon : "people_elderly_60px_bluebox"
 			//},
 			{
-				name : "nfi",
-				alias : "Non-food items",
-				icon : "food_NFI_nonfood_item_60px_bluebox"
+				name : "protection",
+				alias : "Protection",
+				icon : "cluster_protection_60px_bluebox"
 			},
 			{
 				name : "livelihood",
